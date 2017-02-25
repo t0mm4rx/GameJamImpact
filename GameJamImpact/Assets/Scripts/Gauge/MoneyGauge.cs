c17b3fc64828c85257bb1e5849a145475f808754
Pierre CAMILLI <Pierre CAMILLI>
2017-02-25 22:44:39 +0100

    Modification du système d'argent.

---
+++ GameJamImpact/Assets/Scripts/Gauge/MoneyGauge.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyGauge : MonoBehaviour {
+
+    #region money
+
+    [Header("Money")]
+
+    [SerializeField]
+    [Tooltip("Texte qui affichera la quantité d'argent.")]
+    private UnityEngine.UI.Text moneyDisplayer;
+
+    [Tooltip("Quantité d'argent actuelle.")]
+    public int money = 0;
+
+    #endregion
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Display()
+    {
+        if(moneyDisplayer != null)
+            moneyDisplayer.text = "Argent : " + money + " $";
+    }
+
+    public static MoneyGauge operator +(MoneyGauge mg, int m)
+    {
+        mg.money = Mathf.Max(mg.money + m, 0);
+        return mg;
+    }
+
+    public static explicit operator int(MoneyGauge mg)
+    {
+        return mg.money;
+    }
+}
