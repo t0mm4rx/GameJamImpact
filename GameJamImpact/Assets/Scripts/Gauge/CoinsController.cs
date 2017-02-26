83a15500e9db8697b72da3cc8c73d48f60f0c28c
Tom <tom.marx@hotmail.fr>
2017-02-26 16:58:03 +0100

    Ajout musiques

---
+++ GameJamImpact/Assets/Scripts/Gauge/CoinsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsController : MonoBehaviour {
+
+	void Start () {
+		GameObject.Find ("Coins").GetComponent<Text> ().text = PlayerPrefs.GetInt("money") + " €";
+	}
+
+	void Update () {
+		
+	}
+}
